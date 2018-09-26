aa2be9ddce425fc5bfc672bc7328242d5964a858
天天 <xiangbo-99@163.com>
2018-09-26 14:41:15 +0800

    Egg demo add middleware

--- egg-server/config/config.default.js
+++ egg-server/config/config.default.js
@@ -1,11 +1,23 @@
 exports.keys = 'eggs';
+
 exports.view = {
   defaultViewEngine: 'nunjucks',
   mapping: {
     '.tpl': 'nunjucks',
   },
 };
+
+exports.middleware = [
+  'robot',
+];
+
 exports.news = {
   pageSize: 5,
   serverUrl: 'https://hacker-news.firebaseio.com/v0',
+};
+
+exports.robot = {
+  ua: [
+    /Baiduspider/i,
+  ],
 };
